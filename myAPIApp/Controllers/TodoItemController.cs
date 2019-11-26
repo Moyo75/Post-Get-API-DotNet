9e0c8fdb23e689b8c9af8872b01530ab03195044
John Ademoye <moyojohn188@gmail.com>
2019-11-26 12:32:31 +0100

    Add project files.

---
+++ myAPIApp/Controllers/TodoItemController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myAPIApp.Models;
+
+namespace myAPIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemController : ControllerBase
+    {
+
+        private readonly TodoItemDbContext context;
+
+        public  TodoItemController(TodoItemDbContext Context)
+        {
+            context = Context;
+        }
+
+        // Get: api/TodoItem
+        [HttpGet]
+        public async Task<IEnumerable<TodoItem>> GetTodoItems()
+        {
+            return await context.TodoItem.ToListAsync();
+        }
+
+        // Post: api/TodoItem
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoitem)
+        {
+            context.TodoItem.Add(todoitem);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItems), new { id = todoitem.Id }, todoitem);
+        }
+    }
+}
---
+++ myAPIApp/Models/TodoItemDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myAPIApp.Models
+{
+    public class TodoItemDbContext : DbContext
+    {
+        public TodoItemDbContext(DbContextOptions<TodoItemDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItem { get; set; }
+    }
+}
